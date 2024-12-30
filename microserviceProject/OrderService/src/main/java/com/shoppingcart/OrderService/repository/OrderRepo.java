ac5d0753b33f858492db9cb61286e7cf05ca92f3
ajaykaleer <ajaykale21446@gmail.com>
2024-12-30 09:43:15 +0530

    Delete microserviceProject directory

--- microserviceProject/OrderService/src/main/java/com/shoppingcart/OrderService/repository/OrderRepo.java
+++
@@ -1,8 +0,0 @@
-package com.shoppingcart.OrderService.repository;
-
-import com.shoppingcart.OrderService.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepo<order> extends JpaRepository<OrderItem, Long> {
-
-}
